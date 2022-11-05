7c7508654e044c1ab87889fb2b4e904f750e292d
Raskolnikov <jerry.tann@endress.com>
2022-11-05 12:26:47 +0800

    ✨ how to extend functions

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import * as ALY from 'aliyun-sdk';
+import ALY from 'aliyun-sdk';
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
-console.log('ALY.SLS', ALY.default.SLS)
+// console.log('ALY.SLS', ALY.default.SLS)
 
-
-
-
-var sls = new ALY.default.SLS({
+var sls = new ALY.SLS({
     accessKeyId: process.env.ALI_KEY,                         // The AccessKey ID of your Alibaba Cloud account. For more information, see AccessKey pair. An Alibaba Cloud account has permissions to call all API operations. If you use the AccessKey pair of an Alibaba Cloud account, security risks may occur. We recommend that you create and use a RAM user to call API operations or perform routine O&M.
     secretAccessKey: process.env.ALI_SECRET,                     // The AccessKey secret of your Alibaba Cloud account.
     endpoint: 'http://cn-shanghai.log.aliyuncs.com', // The Log Service endpoint. For more information, see Endpoints. In this example, the Log Service endpoint for the China (Hangzhou) region is used. Replace the parameter value with the actual endpoint.
@@ -31,6 +28,19 @@ var logStoreName = "aws";
 //       }]
 //   }]
 // };
+
+const ass = {
+  fn1: (level, msg) => {
+  console.log(`level ${level}, msg: ${msg}`)}
+}
+
+const base = ass.fn1;
+ass.fn1 = function() {
+  base.apply(this,arguments)
+  console.log(`to ali ${arguments[0]} and ${arguments[1]}`)
+}
+
+ass.fn1('info', 'fakku')
 
 function custom_log(msg) {
   console.log(msg)
